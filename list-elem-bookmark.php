5de9941479891b455ea9c391c90da44b6d82f706
Gautier-Antoine <gautier.antoine88@gmail.com>
2024-02-27 12:13:27 +0100

    february 27

--- list-elem-bookmark.php
+++ list-elem-bookmark.php
@@ -9,7 +9,6 @@
 
 /**
  * Trigger Shortcode
- * [orchestrated_contact_form recipients="antoine@orchestrated.ca, support@orchestrated.ca"]
  *
  * @param array attributes
  * @return string|false
@@ -31,21 +30,88 @@ function block_shortcode()
  */
 function shortcode_block_init()
 {
-    add_shortcode('viscawebsc', 'block_shortcode');
+    add_shortcode('viscaweb-sc', 'block_shortcode');
 }
 add_action('init', 'shortcode_block_init');
 
-
-
+/**
+ * Make HTML
+ *
+ * @return void
+ */
+function visual_elem() {
+    $plugin_url = plugin_dir_url( __FILE__ );
+    ?>
+    <div class="block-best-betting-site">
+        <div class="header flex">
+            <a class="sort" onClick="sort_elem(this)">Sort Alphabetically</a>
+            <div class="title flex">
+                <img class="logo" src="<?= $plugin_url; ?>./img/canada.png" alt="canadian flag">
+                <h1>BEST SPORTS BETTING SITES</h1>
+            </div>
+            <div class="view-content flex hide-mobile">
+                <label for="view" class="view">Change View</label>
+                <input type="checkbox" onClick="change_view()" name="view" id="view" class="view">
+            </div>
+        </div>
+        <hr class="hide-mobile">
+        <div class="main flex">
+            <?php
+            $elems = get_block_details();
+            $html = '';
+            foreach ($elems as $elem) {
+                $id = sanitize_title_with_dashes($elem['name']);
+                $html .= '<div onClick="get_link(this)" class="elem flex" id="' . $id . '">';
+                    $html .= '<div class="part-1 flex">';
+                        $html .= '<h2 class="position">' . $elem['position']['canada'] . '</h2>';
+                        $html .= '<img class="logo" src="' . $plugin_url . $elem['icon'] . '">';
+                        $html .= '<a class="rating flex hide-mobile" href="' . $elem['link_review'] . '">';
+                            for ($i = 0; $i < $elem['rating']; $i++ ) {
+                                $html .= '<img src="' . $plugin_url . './img/icon/star.png">';
+                            }
+                        $html .= '</a>';
+                        $html .= '<a class="review" href="' . $elem['link_review'] . '"></a>';
+                    $html .= '</div>';
+                    $html .= '<div class="part-2 flex">';
+                        if ($elem['bonus']['exclusive']) {
+                            $html .= '<p class="exclusive hide-mobile">- Exclusive -</p>';
+                        } else {
+                            $html .= '<p class="sign-up hide-mobile">100% Sign Up Bonus</p>';
+                        }
+                        $html .= '<p>' . $elem['bonus']['amount'] . '</p>';
+                        if ($elem['bonus']['type'] == 'bet') {
+                            $html .= '<p class="bet hide-mobile"> Free Bet</p>';
+                        } else {
+                            $html .= '<p class="bet hide-mobile"> Bonus</p>';
+                        }
+                        $html .= '<a class="btn flex" href="' . $elem['link'] . '"><span class="hide-mobile">Play Now</span></a>';
+                    $html .= '</div>';
+                $html .= '</div>';
+            }
+            $html .= '</div>';
+            echo $html;
+            ?>
+        <div class="footer">
+            <a class="alpha" href="/all">View All Sports Betting Sites</a>
+        </div>
+    </div>
+    <?php
+}
+/**
+ * Lists of companies
+ *
+ * @return Array
+ */
 function get_block_details() {
-    return $array = [
+    return [
         [
-            'name' => 'Betty',
-            'icon' => 'https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6087f2b060c7a92408bac811_logo.svg',
-            'rating' => 'https://media.istockphoto.com/id/1072182672/vector/five-stars-rating-vector-icon.jpg?s=612x612&w=0&k=20&c=xrQFBRAmMApyDvVOeJrlwYazCNMj52aDwekhVCt_Nu8=',
+            'name' => 'Sports interaction',
+            'icon' => './img/logo/sports-interaction.png',
+            'rating' => 5,
             'link_review' => '/#link-review',
             'bonus' => [
-                'before' =>'200$',
+                'exclusive' => true,
+                'type' =>'bonus',
                 'amount' =>'200$',
                 'after' =>'200$',
             ],
@@ -56,12 +122,13 @@ function get_block_details() {
             ],
         ],
         [
-            'name' => 'Allin',
-            'icon' => 'https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6087f2b060c7a92408bac811_logo.svg',
-            'rating' => 'https://media.istockphoto.com/id/1072182672/vector/five-stars-rating-vector-icon.jpg?s=612x612&w=0&k=20&c=xrQFBRAmMApyDvVOeJrlwYazCNMj52aDwekhVCt_Nu8=',
+            'name' => 'PowerPlay Sports',
+            'icon' => './img/logo/powerplay-sports.png',
+            'rating' => 5,
             'link_review' => '/#link-review',
             'bonus' => [
-                'before' =>'200$',
+                'exclusive' => true,
+                'type' =>'bonus',
                 'amount' =>'200$',
                 'after' =>'200$',
             ],
@@ -72,12 +139,13 @@ function get_block_details() {
             ],
         ],
         [
-            'name' => 'Castor',
-            'icon' => 'https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6087f2b060c7a92408bac811_logo.svg',
-            'rating' => 'https://media.istockphoto.com/id/1072182672/vector/five-stars-rating-vector-icon.jpg?s=612x612&w=0&k=20&c=xrQFBRAmMApyDvVOeJrlwYazCNMj52aDwekhVCt_Nu8=',
+            'name' => 'Betway',
+            'icon' => './img/logo/betway.png',
+            'rating' => 5,
             'link_review' => '/#link-review',
             'bonus' => [
-                'before' =>'200$',
+                'exclusive' => false,
+                'type' =>'bonus',
                 'amount' =>'200$',
                 'after' =>'200$',
             ],
@@ -87,41 +155,39 @@ function get_block_details() {
                 'us' => 2,
             ],
         ],
+        [
+            'name' => 'Spin Sports',
+            'icon' => './img/logo/spin-sports.png',
+            'rating' => 5,
+            'link_review' => '/#link-review',
+            'bonus' => [
+                'exclusive' => true,
+                'type' =>'bet',
+                'amount' =>'500$',
+                'after' =>'200$',
+            ],
+            'link' => '/#link',
+            'position' => [
+                'canada' => 4,
+                'us' => 2,
+            ],
+        ],
+        [
+            'name' => 'Bodog',
+            'icon' => './img/logo/bodog.png',
+            'rating' => 5,
+            'link_review' => '/#link-review',
+            'bonus' => [
+                'exclusive' => false,
+                'type' =>'bonus',
+                'amount' =>'200$',
+                'after' =>'200$',
+            ],
+            'link' => '/#link',
+            'position' => [
+                'canada' => 5,
+                'us' => 2,
+            ],
+        ],
     ];
 }
-
-function visual_elem() {
-    ?>
-    <div class="block-best-betting-site">
-        <div class="header">
-            <a class="sort" onClick="sort_elem(this)">Sort Alphabetically</a>
-            <h2 class="canada">BEST SPORTS BETTING SITES</h2>
-            <input type="checkbox" onClick="change_view()" name="view" id="view" class="view">
-            <label for="view" class="view">Change View</label>
-        </div>
-        <div class="main">
-            <?php
-            $elems = get_block_details();
-            foreach ($elems as $elem) {
-                echo '<div onClick="get_link(this)" class="elem" id="' . sanitize_title_with_dashes($elem['name']) . '">';
-                    echo '<div class="part-1">';
-                        echo '<h2 class="position">' . $elem['position']['canada'] . '</h2>';
-                        echo '<img class="logo" src="' . $elem['icon'] . '">';
-                        echo '<a href="' . $elem['link_review'] . '"><img class="rating" src="' . $elem['rating'] . '"></a>';
-                        echo '<a href="' . $elem['link_review'] . '">Read review</a>';
-                    echo '</div>';
-                    echo '<div class="part-2">';
-                        echo '<p>' . $elem['bonus']['amount'] . '</p>';
-                        echo '<a class="btn" href="' . $elem['link'] . '"><span>Play Now</span></a>';
-                    echo '</div>';
-                echo '</div>';
-            }
-            echo '</div>';
-            ?>
-        <div class="footer">
-            <a class="alpha" href="/all">View All Sports Betting Sites</a>
-        </div>
-    </div>
-    <?php
-}
-
